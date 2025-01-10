61a5e4e168d1dbf248119a1b63e258dd4d81f5d6
SYEONIH <syeonih@naver.com>
2025-01-10 18:29:37 +0900

    FloatingBox

---
+++ src/components/FloatingBox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const FloatingBox = () => {
+  return (
+    <div className="fixed bottom-5 right-5 flex flex-col items-center">
+      <div className="bg-white w-full p-4 ">
+        {/* 제목 */}
+        <h3 className="text-base font-medium mb-[18px] flex flex-col items-center ">최근 본 상품</h3>
+        <div className="space-y-4">
+          {/* 상품 아이템 1 */}
+          <div className="bg-gray-300 w-28 h-28 p-2 cursor-pointer flex flex-col justify-end items-start">
+            <p className="text-xs-10px text-black">라라스윗 초코우유</p>
+          </div>
+          {/* 상품 아이템 2 */}
+          <div className="bg-gray-300 w-28 h-28 p-2 cursor-pointer flex flex-col justify-end items-start">
+            <p className="text-xs-10px text-black">라라스윗 초코우유</p>
+          </div>
+          {/* 상품 아이템 3 */}
+          <div className="bg-gray-300 w-28 h-28 p-2 cursor-pointer flex flex-col justify-end items-start">
+            <p className="text-xs-10px text-black">라라스윗 초코우유</p>
+          </div>
+        </div>
+      </div>
+      {/* TOP 버튼 */}
+      <button className="mt-6 w-16 h-16 bg-gray-300 text-black font-medium text-sm rounded-full">
+        TOP
+      </button>
+    </div>
+  );
+};
+
+export default FloatingBox;
--- src/pages/Home/Main.jsx
+++ src/pages/Home/Main.jsx
@@ -1,1 +1,14 @@
- 
+import React from "react";
+import FloatingBox from "../../components/FloatingBox";
+
+
+function Main() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-500">
+      <h1 className="text-2xl font-bold">여기는 메인입니다</h1>
+      <FloatingBox/>
+    </div>
+  );
+}
+
+export default Main;
