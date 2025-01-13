1b52572bd1068c48c86da0638d19675057272bb2
SYEONIH <syeonih@naver.com>
2025-01-13 17:03:24 +0900

    Category

--- src/pages/Home/Category.jsx
+++ src/pages/Home/Category.jsx
@@ -2,26 +2,33 @@ import React from "react";
 import ProductBox from "../../components/Home/ProductBox";
 import ProductBest from "../../components/Home/ProductBest";
 import SearchHeader from '../../components/SearchHeader';
+import FilterButton from "../../components/Home/FilterButton";
+import FilterDropButton from "../../components/Home/FilterDropButton";
 
 const Category = () => {
   return (
-    <div className="w-full h-auto  bg-yellow-50">
-        {/* 뒤로가기 */}
-        <div>
-        <span> 음료</span>
-        </div>
+    <div className="w-full h-auto">
     <SearchHeader />
-      <main className="px-40 [w-110px]">
+      <main className="mx-auto w-[1100px]">
+
+        {/* 뒤로가기 */}
+        <div className="flex mt-16 mb-8 items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="32" viewBox="0 0 19 32" fill="none">
+          <path d="M18.832 2.832L16 0L0 16L16 32L18.832 29.168L5.664 16L18.832 2.832Z" fill="black"/>
+        </svg>
+        <span className="font-bold text-4xl ml-6 "> 음료 </span>
+        </div>
         {/* 배너 */}
         <section className="mb-16">
-          <div className="w-full h-[180px] bg-main01 flex items-center justify-center text-white font-bold text-xl">
+          <div className="w-full h-[180px] bg-buttongray flex items-center justify-center text-white font-bold text-xl">
             <span>배너영역</span>
           </div>
         </section>
+
         {/* BEST 섹션 */}
         <section className="mb-16">
-          <h2 className="text-2xl font-bold mb-8 ">음료 BEST👑</h2>
-          <div className="grid grid-cols-4  bg-slate-500">
+          <h2 className="text-4xl font-bold mb-8 flex justify-center">음료 BEST👑</h2>
+          <div className="flex justify-between items-center">
             <ProductBest />
             <ProductBest />
             <ProductBest />
@@ -34,17 +41,18 @@ const Category = () => {
 
         {/* 전 상품 섹션 */}
         <section className="mb-32">
-          <h2 className="text-2xl font-bold mb-8">전 상품</h2>
+          <h2 className="text-4xl font-bold flex justify-center mb-8">전 상품</h2>
           {/* 필터 메뉴 */}
           <div className="flex items-center justify-between mb-6">
-            <div className="flex space-x-4">
-              <button className="px-4 py-2 bg-buttongray rounded">제품순위</button>
-              <button className="px-4 py-2 bg-buttongray rounded">제품리뷰</button>
-              <button className="px-4 py-2 bg-buttongray rounded">혈당 관리 인증</button>
+            <div className="flex space-x-4 ">
+              <FilterButton label="제로슈거"  />
+              <FilterButton label="제품리뷰"  />
+              <FilterButton label="혈당 관리 인증"  />
+              
             </div>
             <div className="flex space-x-4">
-              <button className="px-4 py-2 bg-buttongray rounded">이번 상품을 찾아요</button>
-              <button className="px-4 py-2 bg-buttongray  rounded">제외할 대체감미료</button>
+              <FilterDropButton label="이번 상품을 찾아요" />
+              <FilterDropButton label="제외할 대체감미료" />
             </div>
           </div>
 
@@ -53,14 +61,14 @@ const Category = () => {
 
            {/* 정렬 메뉴 */}
            <div className="flex items-center justify-between mb-6">
-            <div className="flex space-x-4">
-              <button className="px-4 py-2 ">총 30개</button> 
+            <div className="flex text-xl font-bold">
+              <h2 className="ml-4">총 <span className="text-main01 ">30</span>개</h2> 
             </div>
-            <div className="flex space-x-4">
-              <button className="px-4 py-2 bg-buttongray rounded">인기순</button>
-              <button className="px-4 py-2 bg-buttongray rounded">신상품순순</button>
-              <button className="px-4 py-2 bg-buttongray rounded">리뷰 많은 순</button>
-              <button className="px-4 py-2 bg-buttongray rounded">별점 높은 순</button>
+            <div className="flex space-x-8 font-normal text-base">
+              <button >인기순</button>
+              <button  >신상품순</button>
+              <button >리뷰 많은 순</button>
+              <buttonc className ="pr-4" >별점 높은 순</buttonc>
             </div>
           </div>
  
@@ -68,12 +76,27 @@ const Category = () => {
         <hr className="mb-8 border-t border-gray-300" />
 
           {/* 상품 리스트 */}
-          <div className="grid grid-cols-4 gap-x-6 gap-y-16 bg-slate-500">
-            {Array(12)
-              .fill(null)
-              .map((_, index) => (
-                <ProductBox key={index} />
-              ))}
+          <div className="flex justify-between items-center mb-16">
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+          </div>
+
+          {/* 상품 리스트 */}
+          <div className="flex justify-between items-center mb-16">
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+          </div>
+
+          {/* 상품 리스트 */}
+          <div className="flex justify-between items-center mb-16">
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
           </div>
         </section>
       </main>
