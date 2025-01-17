8a69c7e9383bc29aa73a384ce2f9bff24459e2bf
SYEONIH <syeonih@naver.com>
2025-01-17 18:48:34 +0900

    DetailBox

--- src/components/Detail/RateBox.jsx
+++ src/components/Detail/RateBox.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const RateBox = ({ rating, reviews }) => {
   return (
-    <div className="inline-flex items-center border border-[#e5e5e5] px-4 py-2 ">
-      <span className="flex items-center text-sm">
-        <span className="mr-1  text-main01">★</span>
+    <div className="inline-flex text-2xl font-normal text-center items-center rounded-lg border-[1px] border-[#707070] h-[76px] w-[444px] bg-white">
+      <div className="flex items-center justify-center w-[222px] h-full text-sm">
+        <span className="mr-1 text-main01">★</span>
         {rating}
-      </span>
-      <span className="mx-2 text-[#888]">|</span>
-      <span className="text-sm text-[#4b3832]">리뷰 {reviews}</span>
+      </div>
+      <div className="flex items-center justify-center h-full font-normal text-[#707070] ">|</div>
+      <div className="flex items-center justify-center w-[222px] h-full text-sm text-[#4b3832]">
+        리뷰 {reviews}
+      </div>
     </div>
   );
 };
